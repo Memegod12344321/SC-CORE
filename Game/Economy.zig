1eb1ff8bfd649edafd05c7add9969012b4ae7cb4
Memegod12344321 <aminogames700@gmail.com>
2026-09-16 22:54:56 +0100

    Fix Economy save for Zig 0.16

--- Game/Economy.zig
+++ Game/Economy.zig
@@ -13,17 +13,8 @@ pub const TestPlayer = struct {
         self.account.diamonds = Account.max_diamonds;
     }
 
-    pub fn save(self: *const TestPlayer) !void {
-        try std.fs.cwd().makePath("Game/data");
-        var file = try std.fs.cwd().createFile("Game/data/player.txt", .{ .truncate = true });
-        defer file.close();
-
-        var buf: [256]u8 = undefined;
-        const text = try std.fmt.bufPrint(&buf,
-            "id={d}\nmoney={d}\ndiamonds={d}\nwheat={d}\n",
-            .{ self.account.id, self.account.money, self.account.diamonds, self.account.wheat },
-        );
-        try file.writeAll(text);
+    pub fn save(self: *const TestPlayer, io: std.Io) !void {
+        try self.account.save(io);
     }
 
     pub fn debugPrint(self: *const TestPlayer) void {
